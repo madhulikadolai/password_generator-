0401d0d53a760b01a0aa82f7d2772aa5c00220dd
madhulikadolai <madhulikadolai@gmail.com>
2026-08-25 23:16:27 +0530

    Password_generator.py

--- Password_generator.py
+++ Password_generator.py
@@ -3,16 +3,23 @@ import string
 
 print("PASSWORD GENERATOR")
 
-length = int(input("Enter password length: "))
+while True:
+    length = int(input("Enter password length: "))
 
-if length < 4:
-    print("Password length should be at least 4.")
-else:
-    characters = string.ascii_letters + string.digits + string.punctuation
+    if length < 4:
+        print("Password length should be at least 4.")
+    else:
+        characters = string.ascii_letters + string.digits + string.punctuation
 
-    password = ""
+        password = ""
 
-    for i in range(length):
-        password += random.choice(characters)
+        for i in range(length):
+            password += random.choice(characters)
 
-    print("Generated Password:", password)
+        print("Generated Password:", password)
+
+    again = input("Do you want to generate another password? (yes/no): ")
+
+    if again.lower() == "no":
+        print("Thank you!")
+        break
